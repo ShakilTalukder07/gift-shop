d0662db457adb3c8f8c5d9510a6718f512a3dc86
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-04-08 00:43:05 +0600

    slider add on banner section

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import giftBox from '../../../assets/giftBox.png'
+import giftBox1 from '../../../assets/giftBox.png';
+import giftBox2 from '../../../assets/banner2.png';
+import giftBox3 from '../../../assets/banner1.png';
+import giftBox4 from '../../../assets/banner3.png';
+import giftBox5 from '../../../assets/banner2.png';
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Banner = () => {
     return (
         <div>
             <div className="hero bg-[#ECACAB] pb-[50px]">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img className='w-full md:w-1/2 lg:w-1/2 h-full' src={giftBox} alt=''/>
+                    <div className='w-full md:w-1/2 lg:w-1/2 h-full'>
+                        <Swiper
+                            slidesPerView={"auto"}
+                            spaceBetween={0}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            autoplay={{
+                                delay: 1500
+                            }}
+                            navigation
+                            loop={true}
+                            modules={[Pagination, Navigation, Autoplay]}
+
+                            className="mySwiper"
+                        >
+                            <SwiperSlide><img src={giftBox1} alt='' className=' h-[530px]' /></SwiperSlide>
+                            <SwiperSlide><img src={giftBox2} alt='' className=' h-[530px]' /></SwiperSlide>
+                            <SwiperSlide><img src={giftBox3} alt='' className=' h-[530px]' /></SwiperSlide>
+                            <SwiperSlide><img src={giftBox4} alt='' className=' h-[530px]' /></SwiperSlide>
+                            <SwiperSlide><img src={giftBox5} alt='' className=' h-[530px]' /></SwiperSlide>
+                        </Swiper>
+                    </div>
                     <div className='w-full md:w-1/2 lg:w-1/2'>
                         <h1 className="text-5xl font-bold">Find Your Own Happiness</h1>
                         <p className="py-6">The best gifts for those you love. Desire gift delivery daily <br /> and around a clock.</p>
