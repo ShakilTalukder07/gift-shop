ab7410466b0eadc4357f0e7781003aa36c0e657b
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-04-07 20:04:31 +0600

    order system add

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import ItemBanner from '../ItemBanner/ItemBanner';
+import OrderSystem from '../orderSystem/OrderSystem';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <ItemBanner></ItemBanner>
+            <OrderSystem></OrderSystem>
         </div>
     );
 };
