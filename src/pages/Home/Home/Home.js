491a059c66d120f5153325b1324594d07a9c7d18
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-04-07 13:08:32 +0600

    items banner complete

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import ItemBanner from '../ItemBanner/ItemBanner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <ItemBanner></ItemBanner>
         </div>
     );
 };
