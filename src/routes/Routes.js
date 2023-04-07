9085f99eee569af1d11dd3813e47d957f56c17f9
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-04-07 10:47:49 +0600

    basic setup complete

---
+++ src/routes/Routes.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import ErrorElement from "../components/ErrorElement/ErrorElement";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <ErrorElement></ErrorElement>,
+        children: [
+
+        ]
+    }
+])
